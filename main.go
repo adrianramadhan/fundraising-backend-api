5b209d455b20d76e8ee826c44a93f1f7826793ff
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-08-26 20:29:39 +0700

    feat: user repository

--- main.go
+++ main.go
@@ -3,38 +3,12 @@ package main
 import (
 	"fundraising-backend-api/user"
 	"log"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// dsn := "root:root@tcp(127.0.0.1:3306)/fundraising_db?charset=utf8mb4&parseTime=True&loc=Local"
-	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// fmt.Println("Connected to database")
-
-	// var users []user.User
-	// db.Find(&users)
-
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// 	fmt.Println("============")
-	// }
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:root@tcp(127.0.0.1:3306)/fundraising_db?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -42,8 +16,10 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	var users []user.User
-	db.Find(&users)
+	userRepository := user.NewRepository(db)
+	user := user.User {
+		Name: "John Doe",
+	}
 
-	c.JSON(http.StatusOK, users)
+	userRepository.Save(user)
 }

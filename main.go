2809083a701e858a304f79e21697137a46c6d3d3
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-08-26 16:15:43 +0700

    feat: get table data with gorm

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"fundraising-backend-api/user"
 	"log"
 
 	"gorm.io/driver/mysql"
@@ -10,11 +11,27 @@ import (
 
 func main() {
 	dsn := "root:root@tcp(127.0.0.1:3306)/fundraising_db?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connected to database")
+
+	var users []user.User
+	length := len(users)
+
+	fmt.Println(length)
+
+	db.Find(&users)
+
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("============")
+	}
 }

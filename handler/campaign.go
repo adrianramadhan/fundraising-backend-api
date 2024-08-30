fdaf0d4475b32e452d76b634668bf48e5cffa600
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-08-30 20:32:14 +0700

    feat: repository campaigns api

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,1 +1,7 @@
 package handler
+
+// tangkap parameter di handler
+// handler akan memanggil service
+// service yang memanggil repository
+// repository berhubungan dengan db
+// FindAll and FindByUserID

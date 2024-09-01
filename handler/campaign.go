0c69c3e6dc54f709a6118a7e422af3974bd826cb
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-09-01 15:05:01 +0700

    feat: get campaigns handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,7 +1,39 @@
 package handler
 
+import (
+	"fundraising-backend-api/campaign"
+	"fundraising-backend-api/helper"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
 // tangkap parameter di handler
 // handler akan memanggil service
 // service yang memanggil repository
 // repository berhubungan dengan db
 // FindAll and FindByUserID
+
+type campaignHandler struct {
+	service campaign.Service
+}
+
+func NewCampaignHandler(service campaign.Service) *campaignHandler {
+	return &campaignHandler{service}
+}
+
+// api/v1/campaigns
+func (h *campaignHandler) GetCampaigns(c *gin.Context) {
+	userID, _ := strconv.Atoi(c.Query("user_id"))
+
+	campaigns, err := h.service.GetCampaigns(userID)
+	if err != nil {
+		response := helper.APIResponse("Error to get campaigns", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("List of campaigns", http.StatusOK, "success", campaigns)
+	c.JSON(http.StatusOK, response)
+}

99207e205d8c0778e7fa619e9adc367a3826c814
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-09-02 19:24:37 +0700

    feat: get campaign detail handler

--- handler/campaign.go
+++ handler/campaign.go
@@ -32,7 +32,27 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 }
 
 func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	// api/v1/campaigns/:id
 	// handler : mapping id yg di url ke struct input => service, call formatter
 	// service : inputnya struct input => menangkap id dari url, manggil repo
 	// repository : GetCampaignByID
+
+	var input campaign.GetCampaignDetailInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaignDetail, err := h.service.GetCampaignByID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Campaign detail", http.StatusOK, "success", campaignDetail)
+	c.JSON(http.StatusOK, response)
 }

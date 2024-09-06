b6a6e506de2c375374e91df1c454ce5885bf78ae
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-09-06 20:30:33 +0700

    feat: upload campaign image service

--- campaign/input.go
+++ campaign/input.go
@@ -14,3 +14,8 @@ type CreateCampaignInput struct {
 	Perks            string    `json:"perks"`
 	User             user.User `json:"user"`
 }
+
+type CreateCampaignImageInput struct {
+	CampaignID int  `form:"campaign_id" binding:"required"`
+	IsPrimary  bool `form:"is_primary" binding:"required"`
+}

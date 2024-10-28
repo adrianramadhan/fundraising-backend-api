918a193839664a201d68e5364f17396a2adef067
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-10-28 14:08:14 +0700

    chore: add cors

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"strings"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -49,6 +50,7 @@ func main() {
 	transactionHandler := handler.NewTransactionHandler(transactionService)
 
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.Static("/images", "./images")
 	api := router.Group("/api/v1")
 

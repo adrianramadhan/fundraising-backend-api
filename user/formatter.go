653d0dd7c86c0cbcb7319ca7666aab145f01fdbd
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-10-28 14:12:42 +0700

    feat: fetch user endpoint

--- user/formatter.go
+++ user/formatter.go
@@ -6,6 +6,7 @@ type UserFormatter struct {
 	Occupation string `json:"occupation"`
 	Email      string `json:"email"`
 	Token      string `json:"token"`
+	ImageUrl   string `json:"image_url"`
 }
 
 func FormatUser(user User, token string) UserFormatter {
